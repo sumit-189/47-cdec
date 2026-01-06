86d641457e300b65ec8d755600b7ef64f17f7298
Anil Bamnote <anilbamnote21@gmail.com>
2026-01-06 14:32:04 +0530

    vpc-module-added

--- terratf/ec2.tf
+++ terratf/ec2.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 
 
 
 
-resource "aws_instance" "my-ec2"{
-    ami = var.image-id
-    instance_type = var.instance_type
-    key_name = var.my-key
-     # security_groups = ["default"]
-vpc_security_group_ids = [aws_security_group.my-sg.id]
+resource "aws_instance" "my-ec2" {
+  ami           = var.image-id
+  instance_type = var.instance_type
+  key_name      = var.my-key
+  # security_groups = ["default"]
+  vpc_security_group_ids = [aws_security_group.my-sg.id]
 
- user_data = <<-EOT
+  user_data = <<-EOT
     #!/bin/bash
     yum update -y
     yum install httpd -y
@@ -21,31 +21,31 @@ vpc_security_group_ids = [aws_security_group.my-sg.id]
     systemctl enable httpd
     EOT
 
-tags = {
-     Name = "my-server"
-}
+  tags = {
+    Name = "my-server"
+  }
 
 }
 
 variable "image-id" {
-    type = string
-    description = "this var block is for ami id"
-    default = "ami-0f5fcdfbd140e4ab7"
+  type        = string
+  description = "this var block is for ami id"
+  default     = "ami-0f5fcdfbd140e4ab7"
 }
 variable "instance_type" {
-    type = string
-    description = "this var block is for ami id"
-    default = "t3.micro"
+  type        = string
+  description = "this var block is for ami id"
+  default     = "t3.micro"
 }
 
 variable "my-key" {
-    type = string
-    description = "this var block is for ami id"
-    default = "ohio-soheb-key"
+  type        = string
+  description = "this var block is for ami id"
+  default     = "ohio-soheb-key"
 }
 
 output "instance_public_ip" {
-  value = aws_instance.my-ec2.public_ip
+  value       = aws_instance.my-ec2.public_ip
   description = "Public IP address of the instance."
 
 }
--- terratf/sg.tf
+++ terratf/sg.tf
@@ -1,30 +1,30 @@
-resource "aws_security_group" "my-sg"{
+resource "aws_security_group" "my-sg" {
 
-    name = "my-sg2"
-    description = "allow all traffic"
+  name        = "my-sg2"
+  description = "allow all traffic"
 
-    tags = {
-        Name = "my-new-sg"
-    }
+  tags = {
+    Name = "my-new-sg"
+  }
 
-ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-}
-ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-}
-egress {
-    from_port = 0
-    to_port = 0 
-    protocol = "-1"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-}
+  }
 
 
 }
